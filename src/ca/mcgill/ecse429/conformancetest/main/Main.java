6ebe88bd50969801ca0fe853345607c5416baa3b
sfostine <samuel.fostine@mail.mcgill.ca>
2015-10-05 21:25:53 -0400

    Simpler main class

--- src/ca/mcgill/ecse429/conformancetest/main/Main.java
+++ src/ca/mcgill/ecse429/conformancetest/main/Main.java
@@ -12,39 +12,15 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Tree tree = new Tree("ccoinbox.xml");
-		ConformanceTestGenerator ctg = new ConformanceTestGenerator(tree);
-		ctg.makeTheFile();
-		
-		Tree tree1 = new Tree("legislation.xml");
-		ConformanceTestGenerator ctg1 = new ConformanceTestGenerator(tree1);
-		ctg1.makeTheFile();
-		
-		ArrayList<PathTestCase> paths = tree.getAllPathTestCases();
+		String[] filenames = { "ccoinbox.xml", "legislation.xml" };
 
-		for (PathTestCase path : paths) {
+		for (int i = 0; i < filenames.length; i++) {
 
-			ArrayList<Node> arr = path.getPathTestCase();
-			for (int i = 0; i < arr.size(); i++) {
-				System.out.println(arr.get(i).getState() );
-				if(arr.get(i).getTranParent() != null)
-					System.out.println(arr.get(i).getTranParent().getEvent()+"  //  "+arr.get(i).getTranParent().getCondition()+"  //  "+arr.get(i).getTranParent().getAction());
-			}
-			System.out.println("\n\n");
+			Tree tree = new Tree(filenames[i]);
+			ConformanceTestGenerator ctg = new ConformanceTestGenerator(tree);
+			ctg.makeTheFile();
+
 		}
-		
-		ArrayList<PathTestCase> paths1 = tree1.getAllPathTestCases();
 
-		for (PathTestCase path : paths1) {
-
-			ArrayList<Node> arr = path.getPathTestCase();
-			for (int i = 0; i < arr.size(); i++) {
-				System.out.println(arr.get(i).getState() );
-				if(arr.get(i).getTranParent() != null)
-					System.out.println(arr.get(i).getTranParent().getEvent()+"  //  "+arr.get(i).getTranParent().getCondition()+"  //  "+arr.get(i).getTranParent().getAction());
-			}
-			System.out.println("\n\n");
-		}
-		
 	}
 }
